a1e9ec18315be83e6f90c22813f956dbf22724e9
ChaohuiWang3 <wch3145863356@gmail.com>
2021-09-29 21:12:53 +0800

    first commit

---
+++ week 3/homework/q3.2.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int x,y;
+int power;
+int calculatepwoer(int x, int y)
+{
+    int a;
+    a = 1;
+    for(y=0; y<power; y++)
+    {a = a*x;}
+    return a;
+}
+
+int main(void)
+{
+    printf("enter: \n");
+    scanf("%i",&x);
+    for(power=0; power<10; power++)
+    {
+        printf("%i ^ %i = %i\n",x,power,calculatepwoer(x,power));
+    }
+}
